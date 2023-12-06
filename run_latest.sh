1a395642ae2a9a76ad2bd6a486e2034d527d8092
Bryan Castillo <castillo.bryan@gmail.com>
2023-12-06 13:40:39 -0800

    Editing scripts to run problems.

--- run_latest.sh
+++ run_latest.sh
@@ -8,11 +8,18 @@ fi
 
 cargo run --release | tee results/latest.txt
 
+./results_to_csv.sh < results/latest.txt > results/latest.csv
+
 grep -v 'Elapsed Time' results/latest.txt > results/latest_net.txt
 grep -v 'Elapsed Time' results/last.txt > results/last_net.txt
 
-echo "===================================="
-echo "Showing result diffs"
-echo "===================================="
-diff -U 1 results/last_net.txt results/latest_net.txt
+diff_txt=`diff -U 1 results/last_net.txt results/latest_net.txt`
+diff_rc="$?"
 
+if [ $diff_rc -gt 0 ]; then
+    echo "========================================="
+    echo "Differences:"
+    echo "========================================="
+    echo "$diff_txt"
+    echo "========================================="
+fi
