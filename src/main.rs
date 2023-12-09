99919565d27f5760991336cef737d4ef90153fe9
Bryan Castillo <castillo.bryan@gmail.com>
2023-12-09 11:03:45 -0800

    Reformating main to have less repeated text.

--- src/main.rs
+++ src/main.rs
@@ -6,27 +6,24 @@ mod aocio;
 mod run;
 
 fn main() {
-    let problems = vec![
-        problem!(problem1, part1, "input/input_01.txt"),
-        problem!(problem1, part2, "input/input_01.txt"),
-        problem!(problem2, part1, "input/input_02.txt"),
-        problem!(problem2, part2, "input/input_02.txt"),
-        problem!(problem3, part1, "input/input_03.txt"),
-        problem!(problem3, part2, "input/input_03.txt"),
-        problem!(problem4, part1, "input/input_04.txt"),
-        problem!(problem4, part2, "input/input_04.txt"),
-        problem!(problem5, part1, "input/input_05.txt"),
-        problem!(problem5, part2, "input/input_05.txt"),
-        problem!(problem6, part1, "input/input_06.txt"),
-        problem!(problem6, part2, "input/input_06.txt"),
-        problem!(problem7, part1, "input/input_07.txt"),
-        problem!(problem7, part2, "input/input_07.txt"),
-        problem!(problem8, part1, "input/input_08.txt"),
-        problem!(problem8, part2, "input/input_08.txt"),
-        problem!(problem9, part1, "input/input_09.txt"),
-        problem!(problem9, part2, "input/input_09.txt"),
-    ];
-
-    let args: Vec<String> = std::env::args().skip(1).collect();
-    run::run_problems(&problems, &args);
+    run::run_args(problems![
+        problem1::part1,
+        problem1::part2,
+        problem2::part1,
+        problem2::part2,
+        problem3::part1,
+        problem3::part2,
+        problem4::part1,
+        problem4::part2,
+        problem5::part1,
+        problem5::part2,
+        problem6::part1,
+        problem6::part2,
+        problem7::part1,
+        problem7::part2,
+        problem8::part1,
+        problem8::part2,
+        problem9::part1,
+        problem9::part2,
+    ]);
 }
--- src/run.rs
+++ src/run.rs
@@ -1,6 +1,7 @@
-use crate::aocbase::AOCResult;
+use crate::aocbase::{AOCResult, AOCError};
 
 use std::time::Instant;
+use regex::Regex;
 
 pub struct Problem {
     pub name: String,
@@ -10,11 +11,11 @@ pub struct Problem {
 
 impl Problem {
 
-    pub fn run(&self) {
+    pub fn run(&self, input: &String) {
         println!("Running: {}", self.name);
         let start = Instant::now();
 
-        let result = (self.runner)(&self.input);
+        let result = (self.runner)(input);
 
         let duration = start.elapsed();
 
@@ -30,37 +31,60 @@ impl Problem {
     }
 }
 
+pub fn parse_number(name: impl AsRef<str>) -> AOCResult<i32> {
+    Ok(Regex::new(r"(\d+)")?
+        .captures(name.as_ref())
+        .ok_or_else(|| AOCError::ParseError("Name has no number.".into()))?
+        .get(1)
+        .ok_or_else(|| AOCError::InvalidRegexOperation("Invalid group".into()))?
+        .as_str()
+        .parse::<i32>()?)
+}
+
+macro_rules! problems {
+    [$($problem:ident::$part:ident,)*] => {
+        vec![$(problem!($problem::$part),)*]
+    }
+}
+
 macro_rules! problem {
     // Match the pattern for the macro
-    ($problem:ident, $part:ident, $input:expr) => {{
+    ($problem:ident::$part:ident) => {{
         use problems::$problem;
-        use crate::run::Problem;
+        use crate::run::{Problem, parse_number};
 
         let name = format!("{}::{}", stringify!($problem), stringify!($part)).to_string();
+        let problem_n = parse_number(&name).unwrap();
+        let default_input = format!("input/input_{:0>2}.txt", problem_n);
 
         Problem {
             name: name,
-            input: $input.to_string(),
+            input: default_input.to_string(),
             runner: Box::new(|input: &String| $problem::$part(input))
         }
     }}
 }
 
-/// Run the problems where the names have a match in args.
-/// If there are no args, all problems are assumed to be run.
-pub fn run_problems(problems: &Vec<Problem>, args: &Vec<String>) {
-    let mut to_run: Vec<&Problem> = Vec::new();
+/// run_args
+pub fn run_args(problems: Vec<Problem>) {
+    let args: Vec<String> = std::env::args().skip(1).collect();
 
     if args.len() == 0 {
-        to_run.extend(problems.iter());
-    }
-    else {
-        for arg in args.iter() {
-            to_run.extend(problems.iter().filter(|p| p.name.contains(arg)));
+        for p in problems {
+            p.run(&p.input);
         }
     }
-
-    for p in to_run {
-        p.run();
+    else if args.len() == 2 {
+        match problems.iter().find(|p| p.name == args[0]) {
+            None => {
+                panic!("ERROR: Problem: {} does not exist.", args[0]);
+            },
+            Some(p) => {
+                p.run(&args[1]);
+            }
+        }
+    }
+    else {
+        panic!("EROR: Usage <problem> <input>")
     }
 }
